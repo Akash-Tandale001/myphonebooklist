d36035cd7a64d872345c7b8c9de613d7b8be201a
Akash-Tandale001 <ak.tandale123@gmail.com>
2022-02-19 12:28:01 +0530

    prepair deploy

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const path = require('path')
 const dotenv = require('dotenv').config();
 const connectDB = require('./config/db');
 const app = express();
+const path = require('path')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
