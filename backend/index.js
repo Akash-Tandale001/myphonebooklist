c9eee95394782882b3196de318f021eb3be83637
Akash-Tandale001 <ak.tandale123@gmail.com>
2022-02-19 11:46:08 +0530

    static file

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,17 @@ const port = process.env.PORT;
 app.use('/api',require('./routes/route'))
 
 
-app.use("/",express.static(path.join(__dirname, '../frontend/build')));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      )
+    )
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+  }
 
 
 app.listen(port, ()=>{
